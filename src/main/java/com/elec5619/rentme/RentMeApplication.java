d5079ce8c0d36349b22b70cce7492dbf90d05198
Nabin <naabin@outlook.com>
2021-09-01 12:52:28 +1000

    Project template

---
+++ src/main/java/com/elec5619/rentme/RentMeApplication.java
@@ -0,0 +1,13 @@
+package com.elec5619.rentme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elec5619/rentme/RentMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elec5619.rentme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
