8aead543d5e67df007fb663ba4bea18f2b03603d
ar-1999 <89889457+ar-1999@users.noreply.github.com>
2021-10-30 17:32:59 +1100

    stripe api added - aiman

---
+++ src/main/resources/frontend/src/app/stripe-gateway/stripe-gateway.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StripeGatewayComponent } from './stripe-gateway.component';
+
+describe('StripeGatewayComponent', () => {
+  let component: StripeGatewayComponent;
+  let fixture: ComponentFixture<StripeGatewayComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ StripeGatewayComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StripeGatewayComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/main/resources/frontend/src/app/stripe-gateway/stripe-gateway.component.ts
@@ -0,0 +1,56 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-stripe-gateway',
+  templateUrl: './stripe-gateway.component.html',
+  styleUrls: ['./stripe-gateway.component.css']
+})
+export class StripeGatewayComponent {
+    paymentHandler: any = null;
+
+    constructor() {
+    }
+
+    ngOnInit() {
+        this.invokeStripe();
+    }
+
+    makePayment(amount: any) {
+        const paymentHandler = (<any>window).StripeCheckout.configure({
+            key: 'pk_test_51Jof6GJs7OZkYFOstRVxICqKaQ1Na5ujc5BlgP3RkrPcwERYAdjSOX1wR9Vbk83ZDlHnNqfuoRu1th9wAuc3ocJe00Ed3WdPXK',
+            locale: 'auto',
+            token: function (stripeToken: any) {
+                console.log(stripeToken)
+                alert('Stripe token generated!');
+            }
+        });
+
+        paymentHandler.open({
+            name: 'Easy Share',
+            description: 'Rental Appliances',
+            amount: amount * 100
+        });
+    }
+
+    invokeStripe() {
+        if (!window.document.getElementById('stripe-script')) {
+            const script = window.document.createElement("script");
+            script.id = "stripe-script";
+            script.type = "text/javascript";
+            script.src = "https://checkout.stripe.com/checkout.js";
+            script.onload = () => {
+                this.paymentHandler = (<any>window).StripeCheckout.configure({
+                    key: 'pk_test_51Jof6GJs7OZkYFOstRVxICqKaQ1Na5ujc5BlgP3RkrPcwERYAdjSOX1wR9Vbk83ZDlHnNqfuoRu1th9wAuc3ocJe00Ed3WdPXK',
+                    locale: 'auto',
+                    token: function (stripeToken: any) {
+                        console.log(stripeToken)
+                        alert('Payment has been successfull!');
+                    }
+                });
+            }
+
+            window.document.body.appendChild(script);
+        }
+    }
+}
+
